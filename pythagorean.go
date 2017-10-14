f6348b75368e8407c35e6bc2f42af1b0886dc39b
Dan Borges <ahhh.db@gmail.com>
2017-10-14 14:11:28 -0700

    Create  pythagorean.go

---
+++ pythagorean.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+  "math"
+  "fmt"
+  "os"
+  "strconv"
+)
+
+func main(){
+
+  fmt.Println("a^2 + b^2 = c^2")
+  fmt.Println("Arg1 = a; Arg2 = b")
+
+  leg1 := os.Args[1]
+  leg2 := os.Args[2]
+  var err error
+  // Convert our command line strings to Ints
+  var legA, legB int
+  if legA, err = strconv.Atoi(leg1); err != nil {
+    panic(err)
+  }
+
+  if legB, err = strconv.Atoi(leg2); err != nil {
+      panic(err)
+    }
+
+  fmt.Println(int(legA), int(legB))
+  hypotenuse := math.Sqrt(math.Pow(float64(legA),2) + math.Pow(float64(legB),2))
+  fmt.Println(hypotenuse)
+
+}
