2a3c2cfb4e08d5ffd0d7914ef81cedb8084c49eb
Dan Borges <ahhh.db@gmail.com>
2019-05-08 16:05:44 -0700

    Create web_upload_server.go

---
+++ web_upload_server.go
@@ -0,0 +1,47 @@
+# Largly taken from https://gist.github.com/anujsinghwd/a5b07db628012b99f875a431d081a490
+package main
+
+import (
+	"crypto/md5"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"strconv"
+	"text/template"
+	"time"
+)
+
+func main() {
+	http.HandleFunc("/upload", upload)
+	http.ListenAndServe("127.0.0.1:8000", nil)
+}
+
+func upload(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("method:", r.Method)
+	if r.Method == "GET" {
+		crutime := time.Now().Unix()
+		h := md5.New()
+		io.WriteString(h, strconv.FormatInt(crutime, 10))
+		token := fmt.Sprintf("%x", h.Sum(nil))
+
+		t, _ := template.ParseFiles("upload.gtpl")
+		t.Execute(w, token)
+	} else {
+		r.ParseMultipartForm(32 << 20)
+		file, handler, err := r.FormFile("uploadfile")
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		defer file.Close()
+		fmt.Fprintf(w, "%v", handler.Header)
+		f, err := os.OpenFile("./"+handler.Filename, os.O_WRONLY|os.O_CREATE, 0666)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		defer f.Close()
+		io.Copy(f, file)
+	}
+}
